4c1b6193a3b2c1f704a90ff9502a764cb892d93c
wallac7 <walterlevy1@gmail.com>
2017-03-19 15:15:05 -0400

    Item addition code
    Fixing php and javascript errors is haaaaaard.

---
+++ additem.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database= "WaltersDatabase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$postalservice= ($_POST["name"]);
+
+// sql to create table
+$sql = "INSERT INTO todolist (task)
+VALUES (".$postalservice.");";
+
+if ($conn->query($sql)===TRUE) {
+    echo "Table myusers created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+?>
